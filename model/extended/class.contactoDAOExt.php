b10c70deb56cb477451ab2713fb93a9b85a2d729
juandiegorb <juan.drb.99@gmail.com>
2017-06-17 12:12:19 -0500

    buscar completado

--- model/extended/class.contactoDAOExt.php
+++ model/extended/class.contactoDAOExt.php
@@ -3,7 +3,7 @@
 class contactoDAOExt extends contactoDAO {
 
     public function buscar($nombre) {
-        $sql = 'SELECT con_id, con_foto, con_nombre, con_apellido,con_telefono,con_correo FROM contacto WHERE con_nombre LIKE :nombre OR con_apellido LIKE :nombre OR con_telefono LIKE :nombre AND delete_at IS NULL';
+        $sql = 'SELECT con_id, con_foto, con_nombre, con_apellido,con_telefono,con_correo FROM contacto WHERE delete_at IS NULL AND (con_nombre LIKE :nombre OR con_apellido LIKE :nombre OR con_telefono LIKE :nombre)';
         $params = array(
             ':nombre' => '%' . $nombre,
             ':nombre' => $nombre . '%',
